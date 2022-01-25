94f695082f421664b4473d2eb6253d88bc308638
kapil-sharma99 <ks2822123@gmail.com>
2022-01-25 19:30:31 +0530

    Print Statement

--- FirstClass.java
+++ FirstClass.java
@@ -1,5 +1,10 @@
+import javax.sound.sampled.SourceDataLine;
+
 class FirstClass {
   public static void main(String args[]) {
     System.out.println("Hello World!!");
+    System.out.print("HEllo World!!\n");
+    System.out.print("Hello World\n");
+    System.out.print("*\n**\n***\n****");
   }
 }
