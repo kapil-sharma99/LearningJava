d0912057d8bc49f9ad82bff52962f9dc69e601c9
kapil-sharma99 <ks2822123@gmail.com>
2022-01-27 21:41:14 +0530

    Conditional statements in java

--- Conditional.java
+++ Conditional.java
@@ -39,5 +39,29 @@ public class Conditional {
     } else {
       System.out.println("Smaller");
     }
+
+    //Switch Statement
+    int button = sc.nextInt();
+    if(button == 1) {
+      System.out.println("Hello");
+    } else if(button == 2) {
+      System.out.println("Namaste");
+    } else {
+      System.out.println("Bonjour");
+    }
+
+    switch(button) {
+      case 1:
+        System.out.println("Hello");
+        break;
+      case 2:
+        System.out.println("Namaste");
+        break;
+      case 3:
+        System.out.println("Bonjour");
+        break;
+      default:
+        System.out.println("Invalid Input");
+    }
   }
 }
