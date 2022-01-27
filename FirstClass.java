7d28a29b5ecbb8a4988c21668d54caa74f117107
kapil-sharma99 <ks2822123@gmail.com>
2022-01-27 18:08:44 +0530

    Data types in Java

--- FirstClass.java
+++ FirstClass.java
@@ -1,10 +1,24 @@
-import javax.sound.sampled.SourceDataLine;
-
 class FirstClass {
   public static void main(String args[]) {
     System.out.println("Hello World!!");
     System.out.print("HEllo World!!\n");
     System.out.print("Hello World\n");
-    System.out.print("*\n**\n***\n****");
+    System.out.println("*\n**\n***\n****");
+
+    //Variables
+    //String name = "Tony Stark";
+    //System.out.println(name);
+    //int a = 25;
+    //int b = 10;
+    //double price = 25.25;
+    //System.out.println(b);
+    //b = 30;
+    //System.out.println(b);
+
+    int a = 10;
+    int b = 20;
+    int sum = a + b;
+    System.out.println(sum);
+
   }
 }
