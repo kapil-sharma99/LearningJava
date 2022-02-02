e823aceb97cd004fb1a4513e159f056a903e1ebe
kapil-sharma99 <ks2822123@gmail.com>
2022-02-02 07:03:33 +0530

    Pattern Problem - 01 completed

--- Loops.java
+++ Loops.java
@@ -1,9 +1,25 @@
 import java.util.*;
 
-import javax.sql.rowset.spi.SyncResolver;
-
 public class Loops {
   public static void main(String args[]) {
-    System.out.println("Hii");
+    for(int i = 0; i < 10; i++) {
+      System.out.println("Hello World!!");
+    }
+
+    for(int i = 1; i <= 10; i++) {
+      System.out.println(i);
+    }
+
+    int i = 1;
+    while(i < 11) {
+      System.out.println(i);
+      i++;
+    }
+
+    int j = 0;
+    do {
+      System.out.println(j);
+      j++;
+    } while(j < 11);
   }
 }
